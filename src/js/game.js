87ceb69500c2c99a54c5401f719745d20c2afb73
Charles <charllie.1337@gmail.com>
2024-10-25 09:50:58 -0300

    Update game.js

--- src/js/game.js
+++ src/js/game.js
@@ -85,7 +85,7 @@ const createCard = (character) => {
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
 
-    front.style.backgroundImage = url`(../images/${character}.gif)`;
+    front.style.backgroundImage = `url(../images/${character}.gif)`;
     card.appendChild(front);
     card.appendChild(back);
 
